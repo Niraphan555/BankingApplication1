155850ec770752fe04a39b2dd15b43bbdfa3d0f7
Niraphan555 <110714106+Niraphan555@users.noreply.github.com>
2023-05-22 22:05:27 +0700

    The newest file eiei

---
+++ BankingApplication1/java/BankingApplication/Account.java
@@ -0,0 +1,11 @@
+package system;
+
+public class CurrentAccount implements Account{
+    public interface Account {
+        public void deposit(double amount);
+        public void withdraw(double amount);
+        public double getBalance();
+        public String getAccountType();
+        public int getAccountNumber();
+        public String getAccountName();
+    }
